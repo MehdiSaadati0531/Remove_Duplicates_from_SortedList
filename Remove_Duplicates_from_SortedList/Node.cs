eaf162d90a2231f98bcc6b9799deadf84c518587
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-29 17:30:47 +0430

    Add files via upload

---
+++ Remove_Duplicates_from_SortedList/Node.cs
@@ -0,0 +1,55 @@
+﻿namespace Remove_Duplicates_from_SortedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+    }
+
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+                ListNode temp = head;
+                while (temp != null && temp.next != null)
+                {
+                    if (temp.val == temp.next.val)
+                    {
+                        temp.next = temp.next.next;
+                    }
+                    else
+                    {
+                        temp = temp.next;
+                    }
+                }
+                return head;
+        }
+
+        // Befor Clean
+        //public ListNode DeleteDuplicates(ListNode head)
+        //{
+        //    if (head != null)
+        //    {
+        //        ListNode temp = head;
+        //        while (temp.next != null)
+        //        {
+        //            if (temp.val == temp.next.val)
+        //            {
+        //                temp.next = temp.next.next;
+        //                continue;
+        //            }
+        //            if (temp.next == null)
+        //            {
+        //                return head;
+        //            }
+        //            temp = temp.next;
+        //        }
+        //        return head;
+        //    }
+        //    else
+        //    {
+        //        return head;
+        //    }
+        //}
+    }
+}
---
+++ Remove_Duplicates_from_SortedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using Remove_Duplicates_from_SortedList;
+
+
+//ListNode e = new ListNode() {val = 3, next = null };
+//ListNode d = new ListNode() {val = 3, next = e };
+ListNode c = new ListNode() {val = 1, next = null };
+ListNode b = new ListNode() {val = 1, next = c };
+ListNode head = new ListNode() {val = 1, next = b };
+
+Solution a = new Solution();
+
+Console.WriteLine(a.DeleteDuplicates(head));
